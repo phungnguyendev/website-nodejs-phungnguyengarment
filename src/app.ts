9a04f7bf55e0f4dee88378430d43e60198162ecf
phungnguyengarment.dev <165115701+phungnguyendev@users.noreply.github.com>
2024-05-28 17:04:12 +0700

    fix: update

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,7 @@ import sequelize from './api/models'
 
 const app = express()
 
-const corsOptions: CorsOptions = {
-  origin: ['http://localhost:5173']
-}
+const corsOptions: CorsOptions = {}
 // Accept json body request
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
